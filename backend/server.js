1cd7800f76bc3d4f6e72109cb4c45f988ac88a22
suraj371k <surajkushwaha371@gmail.com>
2025-04-07 11:14:06 +0530

    "adding functionality"

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import connectDb from './config/db.js'
 import userRoutes from './routes/user.routes.js'
 import postRoutes from './routes/post.routes.js'
 import notificationRoutes from './routes/notification.routes.js'
+import path from 'path'
+
 const app = express()
 dotenv.config()
 cloudinary.config({
@@ -15,7 +17,7 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-
+const __dirname = path.resolve()
 app.use(express.json({limit: "10mb"}))
 app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
@@ -24,6 +26,14 @@ app.use('/api/auth' , authRoutes)
 app.use('/api/users' , userRoutes)
 app.use('/api/posts' , postRoutes)
 app.use('/api/notifications' , notificationRoutes)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname , "/frontend/dist")))
+
+    app.get("*" , (req , res) => {
+        res.sendFile(path.resolve(__dirname , "frontend" , "dist" , "index.html"))
+    })
+}
 connectDb()
 app.listen(5000 , () => {
     console.log("server is running or port 5000")
